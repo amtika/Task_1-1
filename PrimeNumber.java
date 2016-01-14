781f7eaf51b65a1d844a94cda91976b7ad92dd8b
amtika <amtika27@gmail.com>
2016-01-14 22:43:46 +0700

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+public class PrimeNumber {
+	public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Input: ");
+        int x=in.nextInt();
+        int p=0;
+        for (int i = 1; i < x; i++) {
+            if(x%i==0)
+                p=p+1;
+        }
+        if(p==1)
+            System.out.println("Prima");
+        else   
+            System.out.println("Bukan Prima");
+    }
+}
